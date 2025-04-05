6fe7a6bc5044beded7eb616aea8b35891c5c7f3f
Aviral singh <singhaviral041@gmail.com>
2025-04-05 17:28:22 +0530

    Adding Repository for ProjectAssignment entiy

---
+++ src/main/java/com/example/ems/repositories/ProjectAssignmentRepository.java
@@ -0,0 +1,7 @@
+package com.example.ems.repositories;
+
+import com.example.ems.entities.ProjectAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectAssignmentRepository extends JpaRepository<ProjectAssignment, Long> {
+}

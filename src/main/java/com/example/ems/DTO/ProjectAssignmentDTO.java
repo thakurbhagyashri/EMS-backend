93b4cf7b7cc604ce3aff051bc4279b0ef4d9e0df
Aviral singh <singhaviral041@gmail.com>
2025-04-09 12:29:23 +0530

    code for ProjectAssigment entity

---
+++ src/main/java/com/example/ems/DTO/ProjectAssignmentDTO.java
@@ -0,0 +1,41 @@
+package com.example.ems.DTO;
+
+
+import com.example.ems.entities.ProjectAssignment;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjectAssignmentDTO {
+    private Long assignmentId;
+
+    @NotNull(message = "Employee ID is required")
+    private Long employeeId;
+
+    @NotNull(message = "Project ID is required")
+    private Long projectId;
+
+    @NotNull(message = "Start date is required")
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @NotNull(message = "Allocation percentage is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Allocation percentage must be greater than 0")
+    private BigDecimal allocationPercentage;
+
+    @NotNull(message = "Role is required")
+    private ProjectAssignment.Role role;
+
+    private String responsibilities;
+}
--- src/main/java/com/example/ems/repositories/ProjectAssignmentRepository.java
+++ src/main/java/com/example/ems/repositories/ProjectAssignmentRepository.java
@@ -3,5 +3,9 @@ package com.example.ems.repositories;
 import com.example.ems.entities.ProjectAssignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectAssignmentRepository extends JpaRepository<ProjectAssignment, Long> {
+    List<ProjectAssignment> findByEmployeeId(Long employeeId);
+
 }

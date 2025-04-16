d5b5e79071824f2c9f5e947fb93faa644a8c7bda
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2025-04-16 17:46:26 +0530

    Delete src/main/java/com/example/ems/repositories/PerformanceReviewRepository.java

--- src/main/java/com/example/ems/repositories/PerformanceReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ems.repositories;
-
-import com.example.ems.entities.PerformanceReview;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PerformanceReviewRepository extends JpaRepository<PerformanceReview, Long> {
-    List<PerformanceReview> findByEmployee_EmployeeId(Long employeeId);
-}

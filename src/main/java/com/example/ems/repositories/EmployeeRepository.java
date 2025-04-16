4dfd522f0b7c1ad62e70232af3ce7280918b4d23
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2025-04-16 17:45:39 +0530

    Delete src/main/java/com/example/ems/repositories/EmployeeRepository.java

--- src/main/java/com/example/ems/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ems.repositories;
-
-import com.example.ems.entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}

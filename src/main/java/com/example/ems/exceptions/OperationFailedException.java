95a7bb98c2f561db3e74aef5b33d3de7c19b5c72
Abhay7898 <abhaygarhwal95@gmail.com>
2025-04-19 17:26:09 +0530

    Remove changes from main

--- src/main/java/com/example/ems/exceptions/OperationFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ems.exceptions;
-
-public class OperationFailedException extends RuntimeException {
-    public OperationFailedException(String message) {
-        super(message);
-    }
-}
